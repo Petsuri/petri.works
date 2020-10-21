f9f9453378fefcd7a4c0ab4ad391ea3e9752b1b2
Petsuri <miikinpetri@gmail.com>
2020-10-21 23:19:56 +0300

    Use hosted zone created SOA & NS records and manually update registered domain name servers to make this work

--- infrastructure/modules/networking/route53/main.tf
+++ infrastructure/modules/networking/route53/main.tf
@@ -5,35 +5,6 @@ resource "aws_route53_zone" "domain" {
   }
 }
 
-resource "aws_route53_record" "ns" {
-  allow_overwrite = true
-  name            = var.domain
-  ttl             = 60
-  type            = "NS"
-  zone_id         = aws_route53_zone.domain.zone_id
-
-  records = [
-    "ns-1532.awsdns-63.org.",
-    "ns-100.awsdns-12.com.",
-    "ns-1763.awsdns-28.co.uk.",
-    "ns-686.awsdns-21.net.",
-  ]
-}
-
-resource "aws_route53_record" "soa" {
-  allow_overwrite = true
-  name            = var.domain
-  ttl             = 60
-  type            = "SOA"
-  zone_id         = aws_route53_zone.domain.zone_id
-
-  records = [
-    "ns-1532.awsdns-63.org. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
-  ]
-}
-
-
-
 resource "aws_route53_record" "www_ipv4" {
   zone_id = aws_route53_zone.domain.zone_id
   name    = var.domain
@@ -56,14 +27,6 @@ resource "aws_route53_record" "www_ipv6" {
     zone_id                = var.alias_host_zone_id
     evaluate_target_health = false
   }
-}
-
-resource "aws_route53_record" "www_canme" {
-  zone_id = aws_route53_zone.domain.zone_id
-  name    = var.domain
-  type    = "CNAME"
-  ttl     = 300
-  records = [var.alias_domain]
 }
 
 resource "aws_route53_record" "cert_records" {
