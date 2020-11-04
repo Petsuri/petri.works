89d6efd23b0490586d134ff5ec02b9b7541cc63e
Petsuri <miikinpetri@gmail.com>
2020-11-04 18:17:37 +0200

    Default serverless base app for future lambdas

---
+++ site/serverless/lambdas/helloworld.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import 'source-map-support/register';
+
+export const handler: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello from petri.works!!',
+    }, null, 2),
+  };
+}
