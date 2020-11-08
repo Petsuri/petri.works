dfca139fbb1ff0740a71a92b82b77119b98f0e91
Petsuri <miikinpetri@gmail.com>
2020-11-08 14:44:35 +0200

    added step to install all api-client required modules

--- site/api-client/petri-works-client.ts
+++ site/api-client/petri-works-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const logImportantMessage = async (log: (message: any) => void): Promise<void> => {
-    await axios.get<string>('http://localhost:4000/v1/hello')
+    await axios.get<string>('https://api.petri.works/v1/hello')
         .then(result => log(result.data))
         .catch(error => log(error));
 }
