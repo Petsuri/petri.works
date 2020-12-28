4d10651c7a31eddf365798c259a3e62f1452fe63
Petri Miikki <miikinpetri@gmail.com>
2020-12-28 22:54:49 +0200

    Added result class from functional programming to make it easier to handle errors and use railway oriented programming

---
+++ site/common/Result.ts
@@ -0,0 +1,42 @@
+
+export type SuccessType<S> = {
+    ok: true,
+    value: S,
+};
+
+export function success<T>(value: T): SuccessType<T> {
+    return {
+        ok: true,
+        value: value,
+    };
+}
+
+export type FailureType<E> = {
+    ok: false,
+    error: E,
+}
+
+export function failure<T>(value: T): FailureType<T> {
+    return {
+        ok: false,
+        error: value,
+    };
+}
+
+export type Result<S, E> = SuccessType<S> | FailureType<E>;
+
+export function map<S, E, SS>(current: Result<S, E>, f: (value: S) => SS): Result<SS, E> {
+    if (current.ok) {
+        return success(f(current.value));
+    }
+
+    return current;
+}
+
+export function bind<S, E, SS>(current: Result<S, E>, f: (value: S) => Result<SS, E>): Result<SS, E> {
+    if (current.ok) {
+        return f(current.value);
+    }
+
+    return current;
+}
