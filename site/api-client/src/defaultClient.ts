c64ad57279030c3eb27fbc014e83b0175ad280c5
Petri Miikki <miikinpetri@gmail.com>
2021-01-03 12:59:14 +0200

    Changed to use correct class

--- site/api-client/src/defaultClient.ts
+++ site/api-client/src/defaultClient.ts
@@ -1,8 +1,8 @@
 import { AxiosRequest as AxiosRequest } from "./AxiosRequest";
 import { ApiClient, PetriWorksClient } from "./PetriWorksClient";
-import { Response } from "./Response";
+import { JsonResponse } from "./JsonResponse";
 import axios from "axios";
 
 export const defaultClient = (apiBaseUrl: string): ApiClient => {
-  return new PetriWorksClient(new AxiosRequest(apiBaseUrl, axios), new Response());
+  return new PetriWorksClient(new AxiosRequest(apiBaseUrl, axios), new JsonResponse());
 };
