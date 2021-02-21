be39bf995c6e88b4ccd9d7396355992c99917c62
Petri Miikki <miikinpetri@gmail.com>
2021-02-22 00:05:59 +0200

    Content for technologies page, WIP

---
+++ site/client/src/cv/TechnologyItems.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { UCaseTypography } from "../styles/components";
+
+export type ItemsProps = {
+  readonly Items: string[];
+}
+
+const TechnologyItems = (props: ItemsProps) => {
+  const { Items } = props;
+
+  const toGridItem = (item: string, index: number) => {
+    return (
+      <Grid item key={index}>
+        <UCaseTypography variant="h6" component="h5">{item}</UCaseTypography>
+      </Grid>
+    )
+  }
+
+  return (
+    <Grid container spacing={1} justify="center">
+      {Items.map(toGridItem)}
+    </Grid>
+  )
+};
+
+export default TechnologyItems;
--- site/client/src/cv/TechonologiesPage.tsx
+++ site/client/src/cv/TechonologiesPage.tsx
@@ -1,13 +1,49 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { PageContainer, UCaseTypography } from "../styles/components";
+import { PageContainer, Paragraph, ParagraphContainer, UCaseTypography } from "../styles/components";
+import TechnologyItems from "./TechnologyItems";
+
 
 export default function TechnologiesPage() {
   const { t } = useTranslation();
 
+  const languages = ["C#", "Javascript", "Typescript", "PHP", "VB.NET", "SQL", "HCL"];
+  const infrastructure = ["AWS", "GCP", "Serverless", "Terraform", "Kubernetes", "Docker", "RabbitMQ"];
+  const databases = ["SQLServer", "MariaDB", "MySQL", "DynamoDB", "Redis"];
+  const backend = ["asp.net", "Symfony", "REST", "OpenID", "OAuth", "RobotFramework", "NUnit", "PHPUnit", "Specflow"];
+  const frontend = ["React", "Xamarin.Forms", "Jasmine", "Jest", "Intern"]
+
   return (
     <PageContainer>
       <UCaseTypography variant="h3">{t("technologies.header")}</UCaseTypography>
+      <ParagraphContainer>
+        <UCaseTypography variant="h4">{t("technologies.languages.title")}</UCaseTypography>
+        <Paragraph>{t("technologies.languages.description")}</Paragraph>
+        <Paragraph>{t("technologies.languages.learning")}</Paragraph>
+        <Paragraph>{t("technologies.languages.profiency")}</Paragraph>
+      </ParagraphContainer>
+      <TechnologyItems Items={languages} />
+      <ParagraphContainer>
+        <UCaseTypography variant="h4">{t("technologies.infrastructures.title")}</UCaseTypography>
+        <Paragraph>{t("technologies.infrastructures.description")}</Paragraph>
+        <Paragraph>{t("technologies.infrastructures.aws")}</Paragraph>
+        <Paragraph>{t("technologies.infrastructures.gcp")}</Paragraph>
+        <Paragraph>{t("technologies.infrastructures.azure")}</Paragraph>
+        <Paragraph>{t("technologies.infrastructures.knowledge")}</Paragraph>
+      </ParagraphContainer>
+      <TechnologyItems Items={infrastructure} />
+      <ParagraphContainer>
+        <UCaseTypography variant="h4">{t("technologies.databases.title")}</UCaseTypography>
+      </ParagraphContainer>
+      <TechnologyItems Items={databases} />
+      <ParagraphContainer>
+        <UCaseTypography variant="h4">{t("technologies.backend.title")}</UCaseTypography>
+      </ParagraphContainer>
+      <TechnologyItems Items={backend} />
+      <ParagraphContainer>
+        <UCaseTypography variant="h4">{t("technologies.frontend.title")}</UCaseTypography>
+      </ParagraphContainer>
+      <TechnologyItems Items={frontend} />
     </PageContainer>
   );
 }
