080fa394f3924a974b94ae6a5466cbe36d13ac20
Petri Miikki <miikinpetri@gmail.com>
2021-10-02 22:52:47 +0300

    Fix import so code in imported module won't cause compile errors

--- site/client/src/styles/CodeBlock.tsx
+++ site/client/src/styles/CodeBlock.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 type CodeBlockProps = {
   readonly children: React.ReactNode;
