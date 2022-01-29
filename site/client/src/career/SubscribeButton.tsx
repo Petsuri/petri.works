fef15564336e16b66fcfed58329b620cf798a515
Petri Miikki <miikinpetri@gmail.com>
2022-01-29 17:39:23 +0200

    Remove unused variables / imports

--- site/client/src/career/SubscribeButton.tsx
+++ site/client/src/career/SubscribeButton.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import withTheme from '@mui/styles/withTheme';
 import { StyledButton } from "../components";
 import { styled } from "@mui/material/styles";
 
 import { useTranslation } from "react-i18next";
-
-const Div = styled("div")({});
 
 const FullWidthDiv = styled("div")((props) => ({
   display: "flex",
